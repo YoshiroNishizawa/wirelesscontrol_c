ed7f61e07a2d9ed787bdc248deb1b9125748cbec
YoshiroNishizawa <113979324+YoshiroNishizawa@users.noreply.github.com>
2022-10-26 11:08:08 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (flag == 0 && receivedNumber == 1) {
-        flag = 1
+    if (receivedNumber == 1) {
         pins.servoWritePin(AnalogPin.P0, 45)
         pins.servoWritePin(AnalogPin.P1, 135)
-    }
-    if (flag == 1 && receivedNumber == 1) {
-        pins.servoWritePin(AnalogPin.P0, 90)
-        pins.servoWritePin(AnalogPin.P1, 135)
-    } else if (flag == 1 && receivedNumber == 2) {
-        pins.servoWritePin(AnalogPin.P0, 45)
-        pins.servoWritePin(AnalogPin.P1, 90)
-    } else if (flag == 1 && receivedNumber == 0) {
-        flag = 0
+    } else if (receivedNumber == 2) {
+        pins.servoWritePin(AnalogPin.P0, 135)
+        pins.servoWritePin(AnalogPin.P1, 45)
+    } else if (receivedNumber == 0) {
         pins.servoWritePin(AnalogPin.P0, 90)
         pins.servoWritePin(AnalogPin.P1, 90)
-    } else if (flag == 1) {
-        pins.servoWritePin(AnalogPin.P0, 45)
-        pins.servoWritePin(AnalogPin.P1, 135)
     }
 })
-let flag = 0
 radio.setGroup(1)
-flag = 0
